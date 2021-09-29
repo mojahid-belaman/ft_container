c3d403802381e670b0fede7ee01ecbe4b2e88394
Mojahid Belaman <mbelaman@e2r9p4.1337.ma>
2021-09-29 10:51:52 +0100

    add some function

--- Vector.hpp
+++ Vector.hpp
@@ -5,13 +5,30 @@
 
 namespace ft
 {
+    // NOTE - Category to which the iterator belongs to
+    struct input_iterator_tag {};
+    struct output_iterator_tag {};
+    struct forward_iterator_tag {};
+    struct bidirectional_iterator_tag {};
+    struct random_access_iterator_tag {};
+
+    // TODO - Implement Iterator
+    template < class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T& > class iterator
+    {
+        typedef T           value_type;
+        typedef Distance    difference_type;
+        typedef Pointer     pointer;
+        typedef Reference   reference;
+        typedef Category    iterator_category;
+    };
+
     template <class T, class Alloc = std::allocator<T> >
     class vector
     {
         // ANCHOR - This Is Data Structure Will be Used
         private:
-            int _size;
-            int _capacity;
+            size_t _size;
+            size_t _capacity;
             T*  _arr;
             Alloc _alloc;
         public:
@@ -23,10 +40,12 @@ namespace ft
         typedef T*          pointer;
         typedef const T*    const_pointer;
         typedef size_t      size_type;
+        typedef iterator<random_access_iterator_tag, value_type>        iterator; 
+        typedef iterator<random_access_iterator_tag, const value_type>  const_iterator;
+
         // TODO - Implement Constructor Default (empty)
         vector (const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _arr(nullptr), _alloc(alloc)
         {
-
         }
         // TODO - Implement Constructor Fill (Parametrize)
         vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
@@ -34,12 +53,28 @@ namespace ft
             _size = n;
             _capacity = n;
             _alloc = alloc;
-            _arr = _alloc.allocate(_size);
+            _arr = _alloc.allocate(_capacity);
             for (size_t i = 0; i < _size; i++)
             {
                 _arr[i] = val;
             }
-            
+        }
+        // TODO - Implement Constructor Range (Range Iterators)
+        template <class InputIterator>
+        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0)
+        {
+            _alloc = alloc;
+            while (first != last)
+            {
+                _size++;
+                _capacity++;
+                first++;
+            }
+            _arr = _alloc.allocate(_capacity);
+            for (size_t i = 0; i < _size; i++)
+            {
+                _arr[i] = *(last - 1);
+            }
         }
         // TODO - Implement Constructor the Copy
         vector (const vector& x)
@@ -54,13 +89,18 @@ namespace ft
                 _size = x._size;
                 _capacity = x._capacity;
                 _alloc = x._alloc;
+                _arr = _alloc.allocate(_capacity);
                 for (size_t i = 0; i < _size; i++)
                 {
-                    /* code */
+                    _arr[i] = x._arr[i];
                 }
-                
-
             }
+            return (*this);
+        }
+        //TODO - Implement Destructor (default)
+        ~vector()
+        {
+            delete [] _arr;
         }
     };
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,28 @@
 
 int main()
 {
-    std::vector<int>v1;
-    
+    // NOTE - This Vector the System
+    std::cout << "Vector System:" << std::endl;
+    std::vector<int>v1(4, 1337);
+    // std::cout << v1.size() << std::endl;
+    // std::cout << v1.capacity() << std::endl;
+    // for (size_t i = 0; i < v1.size(); i++)
+    // {
+    //     std::cout << v1[i] << std::endl;
+    // }
+    std::vector<int>v3(v1.begin(), v1.end());
+    std::cout << v3.size() << std::endl;
+    std::cout << v3.capacity() << std::endl;
+    for (size_t i = 0; i < v3.size(); i++)
+    {
+        std::cout << v3[i] << std::endl;
+    }
+
+
+    // NOTE - This My Vector
+    // std::cout << "My Vector:" << std::endl;
+    // ft::vector<int>v2(v1.begin(), v1.end());
+    // ft::vector<int>v3(v2);
+
     return (0);
 }
